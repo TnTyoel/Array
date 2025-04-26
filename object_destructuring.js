c681bc6d76835fbb978bf606dad7b56e438382e1
TnTyoel <yoelguide@gmail.com>
2025-04-26 14:44:17 -0600

    sixth commit

--- object_destructuring.js
+++ object_destructuring.js
@@ -1,9 +1,14 @@
 const person = { name :"Alice", age : 25 };
-const { name, age } = person;
+// const { name, age } = person;
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
 
 const {name: userName, age: userAge } = person;
 console.log(userName);
 console.log(userAge);
+// if a property is missing, You can set a default value
+
+const { name, age = 30} = person
+console.log(name);
+console.log(age);
