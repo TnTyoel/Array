61bdd8fcdec370aafdaf378a35e92388bef5a6a1
TnTyoel <yoelguide@gmail.com>
2025-05-03 14:23:52 -0600

    commit

--- stringMethod_withRegExp.js
+++ stringMethod_withRegExp.js
@@ -3,3 +3,9 @@ console.log("abc123xyz".match(/\d+/));
 console.log("hello world". replace(/world/," JS"));
 console.log("hello1hello2hello3" .split(/\d/));
 console.log(/\d/.test("abc123"));
+
+//Validate Email
+const emailRegex =
+/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+console.log(emailRegex.test("test@example.com"));
+console.log(emailRegex.test("invalid@com"));
