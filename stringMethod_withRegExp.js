98c40701c82bb6050108bf3ca196457481a008a6
TnTyoel <yoelguide@gmail.com>
2025-05-03 14:17:31 -0600

    commit

---
+++ stringMethod_withRegExp.js
@@ -0,0 +1,5 @@
+//String method with RegExp
+console.log("abc123xyz".match(/\d+/));
+console.log("hello world". replace(/world/," JS"));
+console.log("hello1hello2hello3" .split(/\d/));
+console.log(/\d/.test("abc123"));
